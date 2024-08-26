351923f4f015738ce17d3977ce6ec08409029f46
ahmedjammali <93989685+ahmedjammali@users.noreply.github.com>
2024-08-26 16:39:35 +0100

    Update question.js

--- question.js
+++ question.js
@@ -60,6 +60,94 @@ let questions = [
         ]
 
     },
+    {
+        numb: 6,
+        question:"what is ahmed favourite superhero",
+        answer: "spiderman",
+        Options: [
+            "Ironman",
+            "spiderman",
+            "batman",
+            "doctor strange",
+        ]
+
+    },
+    {
+        numb: 7,
+        question:"what is ahmed favourite film",
+        answer: "interstellar",
+        Options: [
+            "interstellar",
+            "forgotten",
+            "inception",
+            "the invisible guest",
+        ]
+
+    },
+    {
+        numb: 8,
+        question:"what is ahmed favourite time to study ki yebde maghsour 9bal exmanet",
+        answer: "5 taa sba7",
+        Options: [
+            "5 taa sba7",
+            "10 taa sba7 ",
+            "5 taa laachiya",
+            "10 taa lil",
+        ]
+
+    },
+
+    {
+        numb: 9,
+        question:"what is ahmed favourite food",
+        answer: "kosksi b lben",
+        Options: [
+            "Ma9rouna",
+            "kosksi b lben",
+            "3de batata bel maadnous w lbsal",
+            "lasania",
+        ]
+
+    },
+
+    {
+        numb: 10,
+        question:"what is ahmed favourite fruit",
+        answer: "Moz",
+        Options: [
+            "3neb",
+            "Dele3",
+            "tefe7",
+            "Moz",
+        ]
+
+    },
+
+    {
+        numb: 11,
+        question:"what is ahmed favourite place to go",
+        answer: "cinéma",
+        Options: [
+            "cinéma",
+            "b7ar",
+            "restaurant",
+            "9ahwa",
+        ]
+
+    },
+
+    {
+        numb: 12,
+        question:"what is ahmed favourite season ",
+        answer: "winter",
+        Options: [
+            "summer",
+            "spring",
+            "winter",
+            "autumn",
+        ]
+
+    },
 
 
 ];
